bcdc61c1cf9a63006d980e2752b3fd85446c2cb5
RUHID <ruhid4406@GMAIL.COM>
2026-02-11 00:27:35 +0400

    rukid.py

---
+++ rukid.py
@@ -0,0 +1,33 @@
+################################################################
+# a = int(input())
+# if a == 0:
+#     print("Proqram is finished")
+# while a != 0:
+#     a = int(input())
+#     if a == 0:
+#         print("Proqram is finished")
+################################################################
+# mebleg = 0
+# sernisin = int(input("Sernisin sayini daxil edin: "))
+# sernisinb = 0
+# while True:
+#     sernisinyas = int(input("Sernisin yasini daxil edin: "))
+#     if sernisinyas <= 4:
+#         mebleg += 0
+#         sernisinb += 1
+#     elif sernisinyas >=5:
+#         mebleg += 10
+#         sernisinb += 1
+#     if sernisinb == sernisin:
+#         print("Umumi mebleg: ", mebleg)
+################################################################
+import random
+# a = random.randint(1, 100)
+# while True:
+#     b = int(input("1-100 arasi eded daxil edin: "))
+#     if b < a:
+#         print("Daxil etdiyiniz eded kicikdir")
+#     elif b > a:
+#         print("Daxil etdiyiniz eded boyukdur")
+#     else:        print("Tebrikler, duz tapdiniz")
+################################################################
